f49d4645780fffb8a07c1950a3d5da1834e8080c
gudivadarahul <gudivadarahul@gmail.com>
2024-06-05 23:54:56 -0500

    fixed problems page

--- src/components/ProblemsPage.jsx
+++ src/components/ProblemsPage.jsx
@@ -2,6 +2,7 @@
 import React, { useContext, useState } from "react";
 import { ProblemsContext } from "../context/ProblemsContext";
 import { Link } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
 
 const ProblemsPage = () => {
   const {
@@ -65,88 +66,110 @@ const ProblemsPage = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h2>Problems</h2>
       <nav>
         <Link to="/dashboard">Dashboard</Link>
       </nav>
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <input
-        type="text"
-        placeholder="Search problems by title..."
-        value={searchTerm}
-        onChange={handleSearch}
-        style={{ marginBottom: "20px", padding: "10px", width: "300px" }}
-      />
-      <form onSubmit={editingProblem ? handleUpdate : handleSubmit}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={editingProblem ? editingProblem.title : newProblem.title}
-          onChange={(e) =>
-            editingProblem
-              ? setEditingProblem({ ...editingProblem, title: e.target.value })
-              : setNewProblem({ ...newProblem, title: e.target.value })
-          }
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          value={
-            editingProblem ? editingProblem.description : newProblem.description
-          }
-          onChange={(e) =>
-            editingProblem
-              ? setEditingProblem({
-                  ...editingProblem,
-                  description: e.target.value,
-                })
-              : setNewProblem({ ...newProblem, description: e.target.value })
-          }
-        />
-        <input
-          type="text"
-          placeholder="Difficulty"
-          value={
-            editingProblem ? editingProblem.difficulty : newProblem.difficulty
-          }
-          onChange={(e) =>
-            editingProblem
-              ? setEditingProblem({
-                  ...editingProblem,
-                  difficulty: e.target.value,
-                })
-              : setNewProblem({ ...newProblem, difficulty: e.target.value })
-          }
-        />
-        <input
-          type="text"
-          placeholder="Tags (comma separated)"
-          value={editingProblem ? editingProblem.tags : newProblem.tags}
-          onChange={(e) =>
-            editingProblem
-              ? setEditingProblem({ ...editingProblem, tags: e.target.value })
-              : setNewProblem({ ...newProblem, tags: e.target.value })
-          }
-        />
-        <button type="submit">
-          {editingProblem ? "Update Problem" : "Add Problem"}
-        </button>
-        {formError && <p style={{ color: "red" }}>{formError}</p>}
-      </form>
-      <ul>
-        {problems.map((problem) => (
-          <li key={problem._id}>
-            <h3>{problem.title}</h3>
-            <p>{problem.description}</p>
-            <p>{problem.difficulty}</p>
-            <p>{problem.tags.join(", ")}</p>
-            <button onClick={() => handleEdit(problem)}>Edit</button>
-            <button onClick={() => deleteProblem(problem._id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <div className="loading-spinner">
+          <Oval height={80} width={80} color="#007bff" ariaLabel="loading" />
+        </div>
+      ) : (
+        <>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <input
+            type="text"
+            placeholder="Search problems by title..."
+            value={searchTerm}
+            onChange={handleSearch}
+            style={{ marginBottom: "20px", padding: "10px", width: "300px" }}
+          />
+          <form onSubmit={editingProblem ? handleUpdate : handleSubmit}>
+            <input
+              type="text"
+              placeholder="Title"
+              value={editingProblem ? editingProblem.title : newProblem.title}
+              onChange={(e) =>
+                editingProblem
+                  ? setEditingProblem({
+                      ...editingProblem,
+                      title: e.target.value,
+                    })
+                  : setNewProblem({ ...newProblem, title: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Description"
+              value={
+                editingProblem
+                  ? editingProblem.description
+                  : newProblem.description
+              }
+              onChange={(e) =>
+                editingProblem
+                  ? setEditingProblem({
+                      ...editingProblem,
+                      description: e.target.value,
+                    })
+                  : setNewProblem({
+                      ...newProblem,
+                      description: e.target.value,
+                    })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Difficulty"
+              value={
+                editingProblem
+                  ? editingProblem.difficulty
+                  : newProblem.difficulty
+              }
+              onChange={(e) =>
+                editingProblem
+                  ? setEditingProblem({
+                      ...editingProblem,
+                      difficulty: e.target.value,
+                    })
+                  : setNewProblem({ ...newProblem, difficulty: e.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Tags (comma separated)"
+              value={editingProblem ? editingProblem.tags : newProblem.tags}
+              onChange={(e) =>
+                editingProblem
+                  ? setEditingProblem({
+                      ...editingProblem,
+                      tags: e.target.value,
+                    })
+                  : setNewProblem({ ...newProblem, tags: e.target.value })
+              }
+            />
+            <button type="submit">
+              {editingProblem ? "Update Problem" : "Add Problem"}
+            </button>
+            {formError && <p style={{ color: "red" }}>{formError}</p>}
+          </form>
+          <ul>
+            {problems.map((problem) => (
+              <li key={problem._id}>
+                <h3>{problem.title}</h3>
+                <p>{problem.description}</p>
+                <p>{problem.difficulty}</p>
+                <p>{problem.tags.join(", ")}</p>
+                <button onClick={() => handleEdit(problem)}>Edit</button>
+                <button onClick={() => deleteProblem(problem._id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
